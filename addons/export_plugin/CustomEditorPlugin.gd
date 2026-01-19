7f02ce867075bccb3966788f468cb59e5ba6ba8b
David Snopek <dsnopek@gmail.com>
2026-01-19 14:11:12 -0600

    Update to godot_openxr_vendors 4.3.0 and adjust settings
    There was an `EditorExportPlugin` in `addons/export_plugin` that added
    some Meta-specific `AndroidManifest.xml` entries, which I assume was
    used to avoid including Meta's proprietary loader. In the latest
    godot_openxr_vendors, the Khronos loader is used for Meta too, so this
    removes the custom plugin and switches to normal Meta configuration.

--- addons/export_plugin/CustomEditorPlugin.gd
+++ addons/export_plugin/CustomEditorPlugin.gd
@@ -6,7 +6,6 @@
 extends EditorPlugin
 
 var plugin_control : Panel
-var export_plugin : EditorExportPlugin
 
 
 func _enter_tree() -> void:
@@ -15,21 +14,11 @@ func _enter_tree() -> void:
 		EditorInterface.get_editor_main_screen().add_child(plugin_control)
 		plugin_control.visible = false
 
-	if not export_plugin:
-		export_plugin = EditorExportPlugin.new()
-		export_plugin.set_script(load("res://addons/export_plugin/CustomEditorExportPlugin.gd"))
-	
-	add_export_plugin(export_plugin)
-
 
 func _exit_tree() -> void:
 	if plugin_control:
 		plugin_control.queue_free()
 		plugin_control = null
-	
-	if export_plugin:
-		remove_export_plugin(export_plugin)
-		export_plugin = null
 
 
 func _get_plugin_name() -> String:
